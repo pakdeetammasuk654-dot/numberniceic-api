17f1d069c8901c3bc8ad6329c4e29a60c8c5ab5c
pakdeetammasuk654-dot <pearphong.th.55@ubu.ac.th>
2025-11-19 03:14:43 +0700

    added view

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        // กำหนด Path ที่มีไฟล์ Template ทั้งหมด
+        "./views/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
